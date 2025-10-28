2a8abf65341ab19c8251f41c583dd59256cbbcfd
aima2014 <zahid.naved45@gmail.com>
2025-10-28 20:37:27 +0500

    Add files via upload

---
+++ activity 3.py
@@ -0,0 +1,13 @@
+number= int(input("Enter Number to Check"))
+print("Number to be checked: ",number)
+
+if number>0:
+    print("The number is Positive")
+    
+elif number<0:
+    print("The number is Negative")
+
+else:
+    print("The number is Zero")
+
+
