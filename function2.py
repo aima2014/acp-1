222ef2971277060bc1741893137ac9d2f09cacc7
aima2014 <zahid.naved45@gmail.com>
2025-11-04 20:45:49 +0500

    Add files via upload

---
+++ function2.py
@@ -0,0 +1,10 @@
+def absolute_value(num):
+    if num>= 0:
+        return num
+    else:
+        return -num
+    
+    print("Absolute value of 7."
+          , absolute_value(7)) 
+    print("Absolute value of -5."
+            , absolute_value(-5))
